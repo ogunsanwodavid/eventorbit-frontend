97d67a4f90b267ded67766a1b865eb45e585bb90
ogunsanwodavid <ogunsanwodavid123@gmail.com>
2025-08-10 12:04:15 +0100

    Initial commit

---
+++ app/components/LayoutContent.tsx
@@ -0,0 +1,7 @@
+export default function LayoutContent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <main>okay {children}</main>;
+}
---
+++ app/global-error.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+//Global error left to default cause I'm weary
+
+export default function GlobalError() {
+  return <div>Global Error</div>;
+}
