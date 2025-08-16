774d6ad993eebb14552a90aee8674ab571410bce
ogunsanwodavid <ogunsanwodavid123@gmail.com>
2025-08-16 09:08:15 +0100

    Fixed session issues

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function ForgotPassword() {
   return <div></div>;
 }
--- app/(auth)/set-password/page.tsx
+++ app/(auth)/set-password/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function SetPassword() {
   return <div></div>;
 }
